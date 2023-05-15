96cbe7ebf4308468a0e3f6f23bbd58fbb9eb551a
apaul <apaul@LAPTOP-U7PS9UDV>
2023-05-15 16:17:17 +0100

    Add project files.

---
+++ VoiceDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoiceDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
